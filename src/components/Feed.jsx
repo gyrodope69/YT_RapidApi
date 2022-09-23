98ab2cc31c252e0f937ef28f78ca46073d1c078e
gyrodope69 <royr91430@gmail.com>
2022-09-22 21:33:30 -0500

    Initial Commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { borderRight } from "@mui/system";
+import { SideBar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.item)
+    );
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: "2" }}>
+        <Typography
+          varient="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+        {/* <Videos videos ={videos}/> */}
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
